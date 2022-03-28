0b45d38a4f0bdf171946026e77514b7fa0d94829
BattleToaster26 <zcbaltzer@gmail.com>
2022-03-28 14:27:28 -0500

    some stuff

--- app/src/main/java/com/example/starwarsplanets/view/MainActivity.kt
+++ app/src/main/java/com/example/starwarsplanets/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.starwarsplanets
+package com.example.starwarsplanets.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.starwarsplanets.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
